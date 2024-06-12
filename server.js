82e7fc3f58c5fbfda71f27d4995df2919da624a6
eder17 <pauloederson09@gmail.com>
2024-06-11 23:15:44 -0300

    modificando server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser";
 import mysql from 'mysql2'
 import jwt from "jsonwebtoken";
 
+
 const SEGREDO = 'REMOTA';
 
+
 const app = express();
-const porta = 3000;
+const porta = 3001;
 const conexao = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -16,35 +18,41 @@ const conexao = mysql.createConnection({
 })
 
 
+
+
 conexao.connect();
+
+
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
+
 app.listen(porta, () => {
-    console.log("servidor rodando e escutando na porta 3000")
+    console.log("servidor rodando e escutando na porta " + porta);
 });
+
+
 
 
 app.get("/", (req, resp) => {
    resp.status(200).send("Nosso servidor da CMO")
 });
 
-app.get("/servicos", (req, res) => {
-  res.status(200).send("Rota para trazer os serviços")
-});
 
-/*
-app.get("/servicos", verificarToken, (req, res) => {
+/*app.get("/servicos", verificarToken, (req, res) => {
   res.status(200).send("Rota para trazer os servicos");
-});
-*/
+});*/
+
+
+
 
 let html = '/';
 
+
 // middleware
-/*function verificarToken(req, res, next){
+function verificarToken(req, res, next){
   const token = req.headers['x-access-token'];
   jwt.verify(token, SEGREDO, (erro, decodificado) => {
     if(erro)
@@ -52,29 +60,37 @@ let html = '/';
     req.id = decodificado.id;
     next();
   });
-}*/
+}
 
-/*app.post("/login", (req, res) => {
 
-let usu = req.body.usuario;
-let sen = req.body.senha;
+app.post("/login", (req, res) => {
+
+
+  let usu = req.body.usuario;
+  let sen = req.body.senha;
+
 
 //conectar ao bd pra buscar o id desse usuario
-if((usu = "marcos") AND (sen = "123")){
-  const id = 1; //isso vem do BD
+  if((usu = "marcos") && (sen = "123")){
+    const id = 1; //isso vem do BD
 
-  //token tem 3 partes = 1) identifica o usuario 2 )segredo 3) opçoes
-  const token = jwt.sign({id}, SEGREDO, { expiresIn: 300 }) //5 min
-  
-  console.log("usuario marcos logou no sistema");
-  res.status.(500).json({autenticado : true, token: token});
 
-}
-else{
-  res.status(501).send("usuario invalido ou inexistente");
-}
+    //token tem 3 partes = 1) identifica o usuario 2 )segredo 3) opçoes
+    const token = jwt.sign({id}, SEGREDO, { expiresIn: 300 }) //5 min
+   
+    console.log("usuario marcos logou no sistema");
+    res.status(500).json({autenticado : true, token: token});
 
-});*/
+
+  }
+  else{
+    res.status(501).send("usuario invalido ou inexistente");
+  }
+
+
+});
+
+
 
 
 app.get("/marcas", (req, res) => {
@@ -87,18 +103,34 @@ app.get("/marcas", (req, res) => {
     </body>
     </html>`
 
+
     res.status(200).send(html);
     //Fazer uma SQL no banco de dados
     //Trazendo as marcas cadastradas e com o fl_marca TRUE
     //lista = html
   });
-  
+ 
+ 
+
+
 app.get("/servicos",(req, res)=>{
+
+
+  var resultado;
+  conexao.query("select * from servico", (err, result) => {
+    if (err) throw err
+    resultado = result;
+    return result;
+})
+
+
+console.log(resultado);
 
 
 
 
 });
+
 
 //Rota para inclusão de novos serviços
 app.post("/servicos",(req, res)=>{
@@ -112,29 +144,36 @@ app.post("/servicos",(req, res)=>{
    
    console.log("titulo: " + tit);
 
+
    conexao.query(
-   `call SP_Ins_servico(?, ?, ?, ?, ?, ?, @id, @msg);`, [tit, desc, img, ativo, url, ordem], (erro, linhas,) => {
+   `call SP_Ins_servico(?, ?, ?, ?, ?, ?, @id, @msg);`, [tit, desc, img, ativo, url, ordem], (erro, linhas) => {
     var campos = {tit, desc, url};
     if(erro) {
+
 
       console.log(erro);
       res.send('Problema ao inserir');
     }
-      
+     
     else{
       console.log(linhas);
       res.send("Serviço inserido!");
     }
-    
-    
+   
+   
+
 
    });
 });
 
 
+
+
 //Servidor criado com JS puro
 
+
 //import http from 'http';
+
 
 //const rotas={
 //   "/": "Servidor criado com Node.js para a disciplina WEB",
@@ -142,13 +181,23 @@ app.post("/servicos",(req, res)=>{
 //   "/marcas": "Apresentação de todas as marcas"
 //};
 
+
 //const server = http.createServer((req, res)  => {
 //    res.writeHead(200, { "Content-type": "text/plain" });
 //    res.end(rotas[req.url]);
 //});
 
+
 //server.listen(3000, () => {
 //    console.log("Servidor escutando porta 3000")
 //})
+
+
+
+
+
+
+
+
 
 
